cb73ad3505e6b283e9deb6c4b5d25100513dbe5b
A1K2S3 <senghaniadarshkishor@gmail.com>
2019-04-02 20:47:25 +0530

    Added TODOS section

---
+++ TODOS/Task.js
@@ -0,0 +1,71 @@
+var Info = {};
+function getInfo(){
+	if(localStorage.length == 0){
+		Info = {
+			Task : [],
+		};
+	}
+	else{
+		Info = JSON.parse(localStorage.getItem('Info'))
+		console.log(Info)
+	}
+}
+
+const putInfo = (Task) => {
+	Info.Task.push(Task);
+	localStorage.setItem('Info', JSON.stringify(Info));
+}
+const displayInfo = () => {
+	
+	id = 1;
+	$('.row').empty();
+	$('.alertHurray').empty();
+	console.log(Info.Task.length);
+	for(let i = 0; i < Info.Task.length; i++){
+		if(Info.Task[i] != undefined){
+			$('.row').append(`
+				<div class=" col col-12 col-lg-3 col-md-4">
+					<div class="card border-secondary mb-3 m-auto  shadow-lg" style="max-width: 18em;">
+						<div class="card-body text-secondary bg-light rounded-lg">
+							<p class="card-text">${Info.Task[i]}</p>
+							<button class="btn btn-danger float-right" onclick="deleteThis(this, this.id)" id="${id}"><i class="far fa-trash-alt"></i></button>
+						</div>
+					</div>
+				</div>
+			`);
+		}
+		id++;
+	}
+	if($('.row').is(':empty')){
+		$('.alertHurray').append(`
+			<div class="alert alert-primary" role="alert">
+				Hurray!! Nothing to do Today
+			</div>
+		`);
+	}
+}
+const mainFunction = () => {
+	let Task = $('#Task').val();
+	getInfo();
+	putInfo(Task);
+	displayInfo();
+	$('#Task').val("")
+}
+
+const deleteThis = (element, id) => {
+	delete Info.Task[id-1];
+	putInfo();
+	displayInfo();
+	element.closest(".col").remove();
+	alertify.error("Task Deleted");
+}
+
+$('document').ready(function(){
+	getInfo();
+	displayInfo();
+	$('#Task').keyup((e)=>{
+		if(e.keyCode === 13){
+			$('#submit').click();
+		}		
+	});
+})
